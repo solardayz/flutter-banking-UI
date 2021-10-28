59878a69e0c4ada96e29e82d5db07062f98c1fad
A <solardayz@naver.com>
2021-10-28 15:49:13 +0900

    다시 만드는 중. 에러 해결!

---
+++ lib/constant/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor = Color(0xff00444f);
+Color kSecondaryColor = Color(0xffff7b67);
+Color kThirdColor = Color(0xfffcd76b);
+Color kBackGroundColor = Color(0xfffff9f4);
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -4,28 +4,89 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  // late CardModel card;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        physics: ClampingScrollPhysics(),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            child: ListView.separated(
-                itemBuilder: (context, index) {
-                  return Container(
-                    child: Text('CARD'),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return SizedBox(
-                    width: 10,
-                  );
-                },
-                itemCount: myCards.length),
+      body: ListView.separated(
+          itemBuilder: (context, index) {
+            return MyCard(
+              card: myCards[index],
+            );
+          },
+          separatorBuilder: (context, index) {
+            return SizedBox(
+              width: 10,
+            );
+          },
+          itemCount: myCards.length),
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  final CardModel card;
+  const MyCard({Key? key, required this.card}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      height: 200,
+      width: 350,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'CARD NAME',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text('JONE'),
+                ],
+              ),
+              Text('XXXX XXXX XXXX 1111'),
+              Row(
+                children: [
+                  Text('EXP DATE'),
+                  Text('22/33'),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    children: [
+                      Text('CVV NUMBER'),
+                      Text('334'),
+                    ],
+                  ),
+                ],
+              ),
+            ],
           ),
-        ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                child: Text('사진'),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
