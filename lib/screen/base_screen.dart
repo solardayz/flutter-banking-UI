e7aa708405e51e1698440ea28495dabd627a9500
A <solardayz@naver.com>
2021-11-02 10:40:12 +0900

    앱바 네비게이션바 완성

--- lib/screen/base_screen.dart
+++ lib/screen/base_screen.dart
@@ -1,3 +1,4 @@
+import 'package:banking/constant/colors.dart';
 import 'package:flutter/material.dart';
 
 import 'home_screen.dart';
@@ -12,13 +13,64 @@ class BaseScreen extends StatefulWidget {
 class _BaseScreenState extends State<BaseScreen> {
   int _selectedIndex = 0;
 
-  List _widgetOptions = [HomeScreen()];
+  List _widgetOptions = [
+    HomeScreen(),
+    HomeScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'My Bank',
+          style: TextStyle(
+            fontFamily: "Poppins",
+            color: kPrimaryColor,
+          ),
+        ),
+        leading: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundImage:
+                NetworkImage("https://placeimg.com/640/480/people"),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.notifications_active,
+              color: Colors.black,
+              size: 27,
+            ),
+          ),
+        ],
+      ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: kPrimaryColor,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Cards',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'OverView',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
       ),
     );
   }
